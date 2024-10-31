be1a7ed247df65bd74ff59badd427381c14bf028
Martin A. Ortega <lynxestudio@outlook.com>
2024-10-31 14:35:24 -0600

    Updates source code

---
+++ src/DestructorSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+Console.Clear();
+Router router = new Router("Cordoba");
+Router router2 = new Router("Monterrey", new
+ byte[] { 10, 3, 0, 0 });
+Console.WriteLine("\n");
+//simulamos un proceso en ejecucción
+Console.WriteLine("\tPrimer router {0}", router.Name);
+Working();
+Console.WriteLine("\tSegundo router {0}", router2.Name);
+Working();
+Console.WriteLine("\tDone!");
+
+static void Working()
+{
+	Console.Write("\ttrabajando [");
+	for (int i = 1; i <= 10; i++)
+		{
+			Console.Write("=");
+			Thread.Sleep(500);
+		}
+	Console.Write(">]");
+	Console.WriteLine("\n");
+}
---
+++ src/DestructorSample/Router.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+public class Router
+{
+	public string Name { private set; get; }
+	public byte[] IPNetwork { private set; get; }
+	public Router(string name)
+	{
+		Name = name;
+		Console.WriteLine("{0} Creado {1}", Name, DateTime.Now);
+	}
+	public Router(string name, byte[] ipnet)
+	{
+		Name = name;
+		IPNetwork = ipnet;
+		Console.WriteLine("{0} Creado {1}", Name, DateTime.Now);
+	}
+
+	~Router()
+	{
+		Console.WriteLine("{0} Destruido {1}", Name, DateTime.Now);
+	}
+}
